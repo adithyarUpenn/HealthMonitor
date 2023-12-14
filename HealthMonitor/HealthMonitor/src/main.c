6d6471fc2c1413b825700be6dfbb77bd57f85d8b
Adithya Rajeev <adithyar@seas.upenn.edu>
2023-12-14 16:35:48 -0500

    Added basic LED Blink command

--- HealthMonitor/HealthMonitor/src/main.c
+++ HealthMonitor/HealthMonitor/src/main.c
@@ -42,12 +42,9 @@ int main (void)
 	/* This skeleton code simply sets the LED to the state of the button. */
 	while (1) {
 		/* Is button pressed? */
-		if (port_pin_get_input_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE) {
-			/* Yes, so turn LED on. */
-			port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
-		} else {
-			/* No, so turn LED off. */
-			port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
-		}
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		delay_ms(500);
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		delay_ms(500);
 	}
 }
